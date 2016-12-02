dc2c2b69250c2214cb928679717e0ce7ca6134e1
Dylanderv <daox008@gmail.com>
2016-12-02 16:12:19 +0100

    ²

--- FestiVite/src/FestiViteBundle/Controller/DefaultController.php
+++ FestiVite/src/FestiViteBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace FestiViteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FestiViteBundle\Entity\Utilisateur;
 
 class DefaultController extends Controller
 {
@@ -29,5 +30,21 @@ class DefaultController extends Controller
     public function rechercheAction()
     {
         return $this->render('FestiViteBundle:Default:recherche.html.twig');
+    }
+
+    public function testAction()
+    {
+        $utilisateur = new Utilisateur();
+        $utilisateur->setNom("Dylan");
+        $utilisateur->setPrenom("Dylan");
+        $utilisateur->setDatenaissance("05/11/1996");
+        //$utilisateur->setAdresseMail("Dylan@aaa.fr");
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($utilisateur);
+        $doctrine->flush();
+
+
+        return $this->render('FestiViteBundle:Default:testDylan.html.twig', array('doctrine' => $doctrine));
     }
 }
