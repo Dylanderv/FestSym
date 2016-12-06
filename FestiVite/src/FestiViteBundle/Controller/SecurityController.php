eeab228b177e7df3e585ea99bc2ad85cdaf227ca
Dylan <daox008@gmail.com>
2016-12-06 18:26:09 +0100

    MDR

--- FestiVite/src/FestiViteBundle/Controller/SecurityController.php
+++ FestiVite/src/FestiViteBundle/Controller/SecurityController.php
@@ -1,27 +1,9 @@
 <?php
-//src/FestiViteBundle/Controller/SecurityController.php
 
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+namespace FestiViteBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class SecurityController extends Controller {
-   /**
-    * @Route("/login", name="login")
-   */
-   public function loginAction(Request $request) {
-      $authenticationUtils = $this->get('security.authentication_utils');
-
-      // get the login error if there is one
-      $error = $authenticationUtils->getLastAuthenticationError();
-
-      // last username entered by the user
-      $lastUsername = $authenticationUtils->getLastUsername();
-
-      return $this->render('FestiViteBundle:Default:pagedaccueil.html.twig', array(
-         'last_username' => $lastUsername,
-         'error' => $error,
-      ));
-   }
+class SecurityController extends Controller
+{
 }
-?>
---
+++ FestiVite/src/FestiViteBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FestiViteBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+}
