aa63dc301076474e84582968435e20b599733bc4
DUMAS Remi <dumasre@hostname.iut2.upmf-grenoble.fr>
2016-12-09 15:21:19 +0100

    script

--- FestiVite/web/css/js/creersoiree.js
+++ FestiVite/web/css/js/creersoiree.js
@@ -1,9 +1,14 @@
+var x = true;
 $("#formule1>a>div").hover(function(){
-  $("#formule1 h1").hide("drop",{direction: 'up'},"slow",function(){});
-  $("#formule1 p").show("drop",{direction: 'down'},"slow");
+  if(x){
+    $("#formule1 h1").hide("drop",{direction: "up"},"slow");
+    $("#formule1 p").show("drop",{direction: "down"},"slow");
+  }
+  x = false;
 },function(){
   $("#formule1 p").hide("drop",{direction: 'down'},"slow");
   $("#formule1 h1").show("drop",{direction: 'up'},"slow");
+  x = true;
 });
 
 $("#formule2>a>div").hover(function(){
