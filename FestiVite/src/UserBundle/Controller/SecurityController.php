b1fd35b924368bce3d2eb5e44525a96e40b0a2f0
Inspirateur <lehype75@gmail.com>
2016-12-09 15:21:05 +0100

    Il était temps que tu traces cette arrête

--- FestiVite/src/UserBundle/Controller/SecurityController.php
+++ FestiVite/src/UserBundle/Controller/SecurityController.php
@@ -9,7 +9,7 @@ class SecurityController extends Controller{
     public function loginAction(Request $request){
         // Si le visiteur est déjà identifié, on le redirige vers l'accueil
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('main');
+            return $this->redirectToRoute('festi_vite_main');
         }
         // Le service authentication_utils permet de récupérer le nom d'utilisateur
         // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
