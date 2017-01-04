94fed02b749d79e7634381c0c0ac0c364306f438
DUMAS Remi <dumasre@hostname.iut2.upmf-grenoble.fr>
2017-01-04 16:53:02 +0100

    MDR ca va merge

--- FestiVite/web/css/js/formsoiree.js
+++ FestiVite/web/css/js/formsoiree.js
@@ -1,6 +1,6 @@
 $("#etapeSuivante").click(function() {
-  var divform = ['#etape1','#etape2','#etape3','#etape4','#etape5'];
-  var chevron = ['#chevron1','#chevron2','#chevron3','#chevron4','#chevron5'];
+  var divform = ['#etape1','#etape2','#etape3'];
+  var chevron = ['#chevron1','#chevron2','#chevron3'];
   var i = 0;
   var stop = 1;
   while( i < divform.length && Boolean(stop)) {
@@ -11,9 +11,9 @@ $("#etapeSuivante").click(function() {
       $(chevron[i+1]).removeClass("chevron hidden-xs").addClass("chevronCurrent");
       if (i == 0) {
         $( "#etapePrecedente").show('medium');
-      } else if (i == 3) {
+      } else if (i == 1) {
         $( "#etapeSuivante").hide('medium');
-        $( "#Finaliser").show('medium');
+        $( "#suivantajout").show('medium');
       }
       stop = 0;
     }
@@ -21,8 +21,8 @@ $("#etapeSuivante").click(function() {
   }
 });
 $("#etapePrecedente").click(function() {
-  var divform = ['#etape1','#etape2','#etape3','#etape4','#etape5'];
-  var chevron = ['#chevron1','#chevron2','#chevron3','#chevron4','#chevron5'] ;
+  var divform = ['#etape1','#etape2','#etape3'];
+  var chevron = ['#chevron1','#chevron2','#chevron3'] ;
   var i = 1;
   var stop = 1;
   while( i < divform.length && Boolean(stop)) {
@@ -31,8 +31,8 @@ $("#etapePrecedente").click(function() {
       $(chevron[i]).removeClass("chevronCurrent").addClass("chevron hidden-xs");
       $(divform[i-1]).show('medium');
       $(chevron[i-1]).removeClass("chevron hidden-xs").addClass("chevronCurrent");
-      if (i == 4) {
-        $( "#Finaliser").hide('medium');
+      if (i == 2) {
+        $( "#suivantajout").hide('medium');
         $( "#etapeSuivante").show('medium');
       } else if (i == 1) {
         $( "#etapePrecedente").hide('medium');
